bff62db609e3bc18462c747e02b3efb7162a930c
David <fernandesc.david@gmail.com>
2020-05-16 13:14:12 -0300

    implementando desafio

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,23 +11,35 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('orders_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   order: Order;
 
+  @Column()
+  @ManyToOne(() => Product, product => product.order_products)
   product: Product;
 
+  @Column()
   product_id: string;
 
+  @Column()
+  @JoinColumn()
   order_id: string;
 
+  @Column()
   price: number;
 
+  @Column()
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
